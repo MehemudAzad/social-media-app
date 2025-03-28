0d19d68dce4e02f06800bd664fd77b38d72e2b03
Mehemud Azad <mr.x@MehemudAzad.local>
2025-03-29 02:03:37 +0600

    navbar dropdown and avatar added

--- travel_guide_app/src/app/(WithCommonLayout)/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-    return (
-      <div>
-        <h1> This is Found items page </h1>
-      </div>
-    );
-}
--- travel_guide_app/src/components/UI/navbar.tsx
+++ travel_guide_app/src/components/UI/navbar.tsx
@@ -16,29 +16,10 @@ import {
 } from "@heroui/navbar";
 import Link from "next/link";
 import { Kbd } from "@heroui/kbd";
+import NavbarDropdown from "./NavbarDropdown";
 
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
@@ -46,7 +27,7 @@ export const Navbar = () => {
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">TechHub</p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -69,6 +50,9 @@ export const Navbar = () => {
       <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full" justify="end">
         <NavbarItem className="hidden sm:flex gap-2">
           <ThemeSwitch />
+        </NavbarItem>
+        <NavbarItem className="hidden sm:flex gap-2">
+          <NavbarDropdown />
         </NavbarItem>
       </NavbarContent>
 
