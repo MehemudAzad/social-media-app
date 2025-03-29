5e06416ee49286970b9b6e96e304302f5bada861
Khalid Hasan Tuhin <khalidhasantuhin@Khalids-MacBook-Air.local>
2025-03-29 22:59:29 +0600

    small changes

--- travel_guide_app/src/app/(WithCommonLayout)/(home)/@RecentPosts/page.tsx
+++ travel_guide_app/src/app/(WithCommonLayout)/(home)/@RecentPosts/page.tsx
@@ -4,19 +4,21 @@ import { Button } from "@heroui/button";
 import Link from "next/link";
 
 export default async function RecentPosts() {
-  const { data: posts } = await getRecentPosts();
-  console.log(posts);
+  // const { data: posts } = await getRecentPosts();
+  // console.log(posts);
+  console.log('first')
 
   return (
     <Container>
       <div className="section-title my-8">
+        <h2>Bangladesh</h2>
         <h2 className="mb-2 text-center text-2xl">Recent Posts</h2>
         <p className="text-center">Discover the latest Tech news</p>
       </div>
       <div className="my-8 grid justify-center gap-10 sm:grid-cols-1 md:grid-cols-4">
-        {posts.map((item: any) => (
+        {/* {posts.map((item: any) => (
           <p key={item._id}>{item.title}</p>
-        ))}
+        ))} */}
       </div>
       <div className="flex justify-center">
         <Button className="rounded-md bg-default-900 text-default" size="md">
